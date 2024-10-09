51c2257a1d39d293332d246bff983832df75784e
CeciliaGarcia15 <cecilia15-5@hotmail.com>
2024-10-09 17:01:05 -0300

    css y usestate

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,5 +1,21 @@
+import React from "react";
+import "./TodoSearch.css";
+
 function TodoSearch() {
-  return <input placeholder="Cortar cebolla" />;
+  const [searchValue, setSearchValue] = React.useState("");
+  console.log("los usuarios buscan" + searchValue);
+  return (
+    <input
+      className="TodoSearch"
+      placeholder="Cortar cebolla"
+      value={searchValue}
+      onChange={(event) => {
+        console.log("escribiste en el todo search");
+
+        setSearchValue(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
