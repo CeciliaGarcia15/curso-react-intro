6d9b866a0d45e61933e19e11de026a063ea83238
Cecilia Garcia <46695480+CeciliaGarcia15@users.noreply.github.com>
2024-10-10 22:38:39 -0300

    fix: se ven los icons dependiendo de la prop que se le pase

--- src/TodoIcon.js
+++ src/TodoIcon.js
@@ -1,7 +1,19 @@
-function TodoIcon() {
+import {ReactComponent as CheckSVG} from './check.svg'
+import {ReactComponent as DeleteSVG} from './delete.svg'
+
+const iconTypes={
+  "check": <CheckSVG/>,
+  "delete":<DeleteSVG/>
+
+}
+function TodoIcon({type}) {
   return(
+    <span className={`Icon Icon-svg Icon-${type}`}>
+      {iconTypes[type]}
+    </span>
     
-  );
+    
+  )
 }
 
 export { TodoIcon };
