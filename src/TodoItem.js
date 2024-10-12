c8d86e201b870b11b627b6568715a8a15ff4123d
Cecilia Garcia <46695480+CeciliaGarcia15@users.noreply.github.com>
2024-10-11 23:50:25 -0300

    cambiar color iconos y colocar funcionalidad

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,22 +6,17 @@ import "./TodoItem.css";
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <CompleteIcon />
-      {/* <span
-        className={`Icon Icon-check ${
-          props.completed && "Icon-check--active"
-        } `}
-        onClick={props.onComplete}
-      >
-        V
-      </span> */}
+      <CompleteIcon 
+        completed={props.completed}
+        onComplete={props.onComplete}
+      />
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <DeleteIcon />
-      {/* <span onClick={props.onDelete} className="Icon Icon-delete">
-        X
-      </span> */}
+      <DeleteIcon 
+      onDelete={props.onDelete}
+      />
+      
     </li>
   );
 }
